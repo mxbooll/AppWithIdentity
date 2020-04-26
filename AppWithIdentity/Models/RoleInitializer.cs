2439df925397998c29e5d6062335954e5dd59285
mxbooll <mxbooll@gmail.com>
2020-04-26 14:38:51 +0500

    roles initialize

---
+++ AppWithIdentity/Models/RoleInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace AppWithIdentity.Models
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            if (await roleManager.FindByNameAsync("группа 1") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("группа 1"));
+            }
+            if (await roleManager.FindByNameAsync("группа 2") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("группа 2"));
+            }            
+        }
+    }
+}
